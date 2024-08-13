44e5a1c580c523143d1786c246a30e66b6442ab0
kaoruyuki <>
2024-08-13 16:17:03 +0900

    Init

---
+++ pages/index.tsx
@@ -0,0 +1,85 @@
+import { NextPage } from "next"
+import { useEffect, useRef } from "react"
+import * as THREE from "three"
+import * as dat from "lil-gui"
+
+const Home : NextPage = () => {
+  const canvasRef = useRef<HTMLElement | null>(null)
+
+  useEffect(() => {
+    const canvas = document.getElementById("canvas") as HTMLElement
+    if (!canvas) return
+    canvasRef.current = canvas
+
+    const gui = new dat.GUI({ width : 300 })
+    gui.show(true)
+
+    const scene = new THREE.Scene()
+    const sizes = {
+      width : innerWidth,
+      height : innerHeight
+    }
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      0.001,
+      1000
+    )
+    const renderer = new THREE.WebGLRenderer({
+      canvas : canvas
+    })
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(window.devicePixelRatio)
+
+    // TODO: Edit
+    // ボックスジオメトリー
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshLambertMaterial({
+      color : "#2497f0"
+    })
+    const box = new THREE.Mesh(geometry, material)
+    box.position.z = -5
+    box.rotation.set(10, 10, 10)
+    scene.add(box)
+    gui.addColor(material, "color")
+
+    // TODO: Edit
+    // ライト
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+    scene.add(ambientLight)
+    const pointLight = new THREE.PointLight(0xffffff, 0.2)
+    pointLight.position.set(1, 2, 3)
+    scene.add(pointLight)
+
+    // アニメーション
+    const clock = new THREE.Clock()
+    const render = () => {
+      const elapsedTime = clock.getElapsedTime()
+      box.rotation.x = elapsedTime
+      box.rotation.y = elapsedTime
+      window.requestAnimationFrame(render)
+      renderer.render(scene, camera)
+    }
+    render()
+
+    // ブラウザのリサイズ操作
+    window.addEventListener("resize", () => {
+      sizes.width = window.innerWidth
+      sizes.height = window.innerHeight
+
+      camera.aspect = sizes.width / sizes.height
+      camera.updateProjectionMatrix()
+
+      renderer.setSize(sizes.width, sizes.height)
+      renderer.setPixelRatio(window.devicePixelRatio)
+    })
+  }, [])
+
+  return (
+    <>
+      <canvas id="canvas"></canvas>
+    </>
+  )
+}
+
+export default Home
